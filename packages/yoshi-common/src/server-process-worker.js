c31784448649486b5d656992c1082bc66907b481
Ran Yitzhaki <ranyitz@gmail.com>
2020-03-03 09:59:33 +0200

    use fast (for ts-node@3.3.0)

--- packages/yoshi-common/src/server-process-worker.js
+++ packages/yoshi-common/src/server-process-worker.js
@@ -1,11 +1,14 @@
+// We use this file in order to have a `fork` with `ipc`
+// and also have `ts-node` register for TypeScript support
+// for the user's server file
+
 const path = require('path');
 
 const serverFilePath = process.env.__SERVER_FILE__;
 
 if (path.extname(serverFilePath) === '.ts') {
   require('ts-node').register({
-    transpileOnly: true,
-    typeCheck: false,
+    fast: true,
   });
 }
 
